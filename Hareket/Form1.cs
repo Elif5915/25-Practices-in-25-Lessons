c9034caad2f24c461c90f043e2779c0fcac500a7
Elif Yıldırım <elifyildirim3@outlook.com>
2024-07-03 21:23:21 +0300

    sixth application

---
+++ Hareket/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Hareket
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Data Source=NETCADYAZ;Initial Catalog=DbNotKayit;Integrated Security=True;Trust Server Certificate=True
+
+        SqlConnection connect = new SqlConnection(@"Data Source=NETCADYAZ;Initial Catalog=Hareket;Integrated Security=True");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("execute HareketProcedure", connect);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+    }
+}
