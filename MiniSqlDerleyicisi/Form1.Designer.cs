bcd8baa0ea95a589e88a96e2d7dcf33ece8dea64
Elif Yıldırım <elifyildirim3@outlook.com>
2024-07-20 23:39:22 +0300

    My 12 apps

---
+++ MiniSqlDerleyicisi/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace MiniSqlDerleyicisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnCalistir = new System.Windows.Forms.Button();
+            this.btnSqlHakkinda = new System.Windows.Forms.Button();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 397);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1303, 294);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1550, 405);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 62);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Çalıştır";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 2);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1019, 387);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // btnCalistir
+            // 
+            this.btnCalistir.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnCalistir.Location = new System.Drawing.Point(1058, 32);
+            this.btnCalistir.Name = "btnCalistir";
+            this.btnCalistir.Size = new System.Drawing.Size(211, 54);
+            this.btnCalistir.TabIndex = 3;
+            this.btnCalistir.Text = "Çalıştır";
+            this.btnCalistir.UseVisualStyleBackColor = false;
+            this.btnCalistir.Click += new System.EventHandler(this.btnCalistir_Click);
+            // 
+            // btnSqlHakkinda
+            // 
+            this.btnSqlHakkinda.BackColor = System.Drawing.SystemColors.Info;
+            this.btnSqlHakkinda.Location = new System.Drawing.Point(1058, 299);
+            this.btnSqlHakkinda.Name = "btnSqlHakkinda";
+            this.btnSqlHakkinda.Size = new System.Drawing.Size(211, 53);
+            this.btnSqlHakkinda.TabIndex = 4;
+            this.btnSqlHakkinda.Text = "SQL Hakkında";
+            this.btnSqlHakkinda.UseVisualStyleBackColor = false;
+            this.btnSqlHakkinda.Click += new System.EventHandler(this.btnSqlHakkinda_Click);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnTemizle.Location = new System.Drawing.Point(1058, 212);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(211, 53);
+            this.btnTemizle.TabIndex = 5;
+            this.btnTemizle.Text = "Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = false;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.button2.Location = new System.Drawing.Point(1058, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(211, 53);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Ekle Sil Güncelle\r\n";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1304, 692);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnSqlHakkinda);
+            this.Controls.Add(this.btnCalistir);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Georgia", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SQL DERLEYİCİSİ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnCalistir;
+        private System.Windows.Forms.Button btnSqlHakkinda;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ MiniSqlDerleyicisi/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace MiniSqlDerleyicisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Data Source=NETCADYAZ;Initial Catalog=DbNotKayit;Integrated Security=True
+
+        SqlConnection connection = new SqlConnection(@"Data Source=NETCADYAZ;Initial Catalog=Hareket;Integrated Security=True");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalistir_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string sorgu = richTextBox1.Text;
+
+                SqlDataAdapter sqlData = new SqlDataAdapter(sorgu, connection);
+                //SqlDataAdapter da = new SqlDataAdapter("select *from Urunler", connectin);
+                DataTable dt = new DataTable();
+
+                sqlData.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Yazmış olduğunuz sorgunuzu kontrol ediniz!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = " ";
+            dataGridView1.DataSource = null;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sorgu = richTextBox1.Text;
+
+            try
+            {
+                connection.Open();
+                SqlCommand komut = new SqlCommand(sorgu, connection);
+                komut.ExecuteNonQuery();
+                connection.Close();
+
+                SqlDataAdapter da = new SqlDataAdapter("select *from urunler", connection);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Yazmış olduğunuz sorgunuzu kontrol ediniz!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+
+
+        }
+
+        private void btnSqlHakkinda_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bu sql derleyici Hareket isimli veritabanına bağlıdır. Bu veritabanında hareketler,musteriler,personel,urunler olmak üzere 4 adet tablo mevcuttur. tabloların önce selectlerini çekerek kolonlar ve neler tuttuğunu görebilirsiniz.Daha sonra üzerinde silme,güncelleme,update işlemleri yapabilirsiniz. Kolay gelsin..:)", "BİLGİLENDİRME", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
