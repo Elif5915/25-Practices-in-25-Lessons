f589b5091044c4960e9d2d47539bf760ec2c46cf
Elif Yıldırım <elifyildirim3@outlook.com>
2024-08-04 16:13:00 +0300

    22 apps
    Bu mini uygulamada RSS nedir ve nasıl kullanılır bunun üzerine öğrenilmek üzere yapılmış bir projedir. Belirli haber sitelerinin rss haber adresleri ile nasıl haber başlıklarının çekildiğini örneklendiren bir uygulamadır.

---
+++ RSS_İnternettenHaberBasliklariCekme/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace RSS_İnternettenHaberBasliklariCekme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnMilliyet = new System.Windows.Forms.Button();
+            this.btnHurriyet = new System.Windows.Forms.Button();
+            this.btnSabah = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnMilliyet
+            // 
+            this.btnMilliyet.Location = new System.Drawing.Point(21, 498);
+            this.btnMilliyet.Name = "btnMilliyet";
+            this.btnMilliyet.Size = new System.Drawing.Size(282, 45);
+            this.btnMilliyet.TabIndex = 0;
+            this.btnMilliyet.Text = "Milliyet";
+            this.btnMilliyet.UseVisualStyleBackColor = true;
+            this.btnMilliyet.Click += new System.EventHandler(this.btnMilliyet_Click);
+            // 
+            // btnHurriyet
+            // 
+            this.btnHurriyet.Location = new System.Drawing.Point(379, 498);
+            this.btnHurriyet.Name = "btnHurriyet";
+            this.btnHurriyet.Size = new System.Drawing.Size(282, 45);
+            this.btnHurriyet.TabIndex = 1;
+            this.btnHurriyet.Text = "Hürriyet";
+            this.btnHurriyet.UseVisualStyleBackColor = true;
+            this.btnHurriyet.Click += new System.EventHandler(this.btnHurriyet_Click);
+            // 
+            // btnSabah
+            // 
+            this.btnSabah.Location = new System.Drawing.Point(735, 498);
+            this.btnSabah.Name = "btnSabah";
+            this.btnSabah.Size = new System.Drawing.Size(282, 45);
+            this.btnSabah.TabIndex = 2;
+            this.btnSabah.Text = "Sabah";
+            this.btnSabah.UseVisualStyleBackColor = true;
+            this.btnSabah.Click += new System.EventHandler(this.btnSabah_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(21, 422);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(282, 70);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(379, 422);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(282, 70);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(736, 422);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(282, 70);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(21, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(996, 404);
+            this.listBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1051, 565);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSabah);
+            this.Controls.Add(this.btnHurriyet);
+            this.Controls.Add(this.btnMilliyet);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "RSS Haber Başlıkları ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMilliyet;
+        private System.Windows.Forms.Button btnHurriyet;
+        private System.Windows.Forms.Button btnSabah;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ RSS_İnternettenHaberBasliklariCekme/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace RSS_İnternettenHaberBasliklariCekme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMilliyet_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            // http://www.milliyet.com.tr/rss/rssNew/gundemRss.xml
+            XmlTextReader xmloku = new XmlTextReader("http://www.milliyet.com.tr/rss/rssNew/gundemRss.xml");
+            while (xmloku.Read())
+            {
+                if (xmloku.Name == "title")
+                {
+                    listBox1.Items.Add(xmloku.ReadString());
+                }
+            }
+
+
+        }
+
+        private void btnHurriyet_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            // http://mix.chimpfeedr.com/07479-Hurriyet-Gazetesi
+            XmlTextReader xmloku1 = new XmlTextReader("http://mix.chimpfeedr.com/07479-Hurriyet-Gazetesi");
+            while (xmloku1.Read())
+            {
+                if (xmloku1.Name == "title")
+                {
+                    listBox1.Items.Add(xmloku1.ReadString());
+                }
+            }
+        }
+
+        private void btnSabah_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            // http://mix.chimpfeedr.com/d1bed-Sabah-Gazetesi
+            XmlTextReader xmloku2 = new XmlTextReader("http://mix.chimpfeedr.com/d1bed-Sabah-Gazetesi");
+            while (xmloku2.Read())
+            {
+                if (xmloku2.Name == "title")
+                {
+                    listBox1.Items.Add(xmloku2.ReadString());
+                }
+            }
+        }
+    }
+}
